2cd728c138539806107cb8faf9291d904ac3d919
Jared Peters <jjpeters@dlfna.com>
2018-01-15 22:57:19 -0800

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var {User} = require('./models/user');
 
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -49,8 +50,8 @@ Todo.findById(id).then((todo) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on ${port}`);
 });
 
 
