1e7a70b9ccbcb4263e416f4bf93f0e4f14ac807b
Jared Peters <jjpeters@dlfna.com>
2018-01-16 21:48:23 -0800

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -33,7 +33,6 @@ app.get('/todos', (req,res) => {
   });
 });
 
-// GET /todos/1234234
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
@@ -60,7 +59,7 @@ Todo.findByIdAndRemove(id).then((todo) => {
   if (!todo) {
     return res.status(404).send();
   }
-    res.send(todo);
+    res.send({todo});
 }).catch((e) => {
     res.status(400).send();
   });
