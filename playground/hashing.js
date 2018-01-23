e350b2908e3076ba6cd10f6de5a584fca1563d48
Jared Peters <jjpeters@dlfna.com>
2018-01-22 21:25:14 -0800

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,28 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//       console.log(hash);
+//   });
+// });
+
+var hashedPassword = "$2a$10$Vo/F/rNDdIT2x2IOdfDEouNr2JTLYv3a1V9lFwsN6B9w1sS75xaXS";
+
+bcrypt.compare("122", hashedPassword, (err, res) => {
+  console.log(res);
+});
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // var message ="I am user number 3";
 // var hash = SHA256(message).toString();
--- server/server.js
+++ server/server.js
@@ -92,6 +92,7 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
+// POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
